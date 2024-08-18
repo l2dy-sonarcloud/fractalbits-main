e2824c9274d3651ae9d81d1113a6bfb5d05267c4
Thomas <thomas.fractalbits@gmail.com>
2024-08-18 08:08:38 -0700

    rpc: parse request and response successfully
    start servers and call curl from client:
    ```
    curl localhost:3000/echo/rpc -XPOST -d "fractalbits.com"
    ```
    
    from nss server console:
    ```
    ➜  playground git:(main) ✗ zig build server
    info(websocket): starting nonblocking worker to listen on 127.0.0.1:9224
    debug(websocket): (127.0.0.1:53192) connected
    debug(websocket): (127.0.0.1:53192) connection successfully upgraded
    debug(websocket): (127.0.0.1:53192) received binary message
    debug(server): rpc method=proto.nss_ops.pb.Method.GetInode
    debug(server): rpc GetInode: key=fractalbits.com
    ```

--- api_server/src/lib.rs
+++ api_server/src/lib.rs
@@ -1,6 +1,10 @@
 mod utils;
 mod ws_client;
 
+use nss_ops::*;
+use prost::Message;
+use ws_client::RpcClient;
+
 #[macro_export]
 macro_rules! io_err {
     [$kind: ident, $msg: expr] => {
@@ -8,40 +12,45 @@ macro_rules! io_err {
     };
 }
 
-pub use ws_client::RpcClient;
-
 pub mod nss_ops {
     include!(concat!(env!("OUT_DIR"), "/nss_ops.rs"));
 }
-use nss_ops::*;
 
 pub async fn nss_put_inode(key: String, value: String) -> PutInodeResponse {
     let mut request = PutInodeRequest::default();
     request.key = key;
     request.value = value;
+    let mut request_bytes = Vec::<u8>::new();
+    request.encode(&mut request_bytes).unwrap();
 
     let mut rpc_client = RpcClient::new("127.0.0.1", 9224).await.unwrap();
-    let response_str = rpc_client.send_request(&request.key).await.unwrap();
+    let response_str = rpc_client.send_request(&request_bytes).await.unwrap();
     assert_eq!(request.key, response_str);
-    rpc_client.close().await.unwrap();
 
     PutInodeResponse {
-        msg_id: 1,
+        id: 1,
         result: Some(put_inode_response::Result::Ok(())),
     }
 }
 
 pub async fn nss_get_inode(key: String) -> GetInodeResponse {
     let mut request = GetInodeRequest::default();
+    request.method = Method::GetInode.into();
+    request.id = 1;
     request.key = key;
+    let mut request_bytes = Vec::<u8>::new();
+    request.encode(&mut request_bytes).unwrap();
 
     let mut rpc_client = RpcClient::new("127.0.0.1", 9224).await.unwrap();
-    let response_str = rpc_client.send_request(&request.key).await.unwrap();
-    assert_eq!(request.key, response_str);
-    rpc_client.close().await.unwrap();
-
-    GetInodeResponse {
-        msg_id: 1,
-        result: Some(get_inode_response::Result::Value("success".to_owned())),
-    }
+    let resp_types = rpc_client.send_request(&request_bytes).await.unwrap();
+    let resp: GetInodeResponse = Message::decode(resp_types.as_bytes()).unwrap();
+    dbg!(&resp);
+    assert_eq!(resp.id, 1);
+    assert_eq!(
+        resp.result,
+        Some(get_inode_response::Result::Ok(
+            "fractalbits.com".to_string()
+        ))
+    );
+    resp
 }
--- api_server/src/ws_client.rs
+++ api_server/src/ws_client.rs
@@ -1,4 +1,3 @@
-use crate::io_err;
 use crate::utils;
 use std::collections::HashMap;
 use std::io::Result;
@@ -11,46 +10,50 @@ use tokio::{
 use web_socket::*;
 
 pub struct RpcClient {
-    requests: Arc<RwLock<HashMap<u64, oneshot::Sender<Event>>>>,
-    ws: WebSocket<BufReader<TcpStream>>,
+    requests: Arc<RwLock<HashMap<u64, oneshot::Sender<Box<[u8]>>>>>,
+    ws: Arc<RwLock<WebSocket<BufReader<TcpStream>>>>,
     next_id: u64,
 }
 
 impl RpcClient {
     pub async fn new(url: &str, port: u32) -> Result<Self> {
-        let ws = utils::connect(&format!("{url}:{port}"), "/").await?;
+        let ws = Arc::new(RwLock::new(
+            utils::connect(&format!("{url}:{port}"), "/").await?,
+        ));
+        let requests = Arc::new(RwLock::new(HashMap::new()));
+
         {
-            // TODO: spawn tokio task for event loop, send results to oneshot channel
-            // tokio::spawn(async move {
-            //     while let Some(message_result) = ws_rx.next().await {
-            //         match message_result {
-            //             Ok(message) => {
-            //                 if let Err(e) =
-            //                     Self::handle_websocket_message(&streams, &requests, message).await
-            //                 {
-            //                     log::error!("{}", e);
-            //                 }
-            //             }
-            //             Err(e) => {
-            //                 log::error!("{}", e);
-            //             }
-            //         }
-            //     }
-            // });
+            let ws_clone = ws.clone();
+            let requests_clone = requests.clone();
+            tokio::spawn(async move {
+                loop {
+                    match ws_clone.write().await.recv().await {
+                        Ok(Event::Data { ty, data }) => {
+                            assert!(matches!(ty, DataType::Complete(MessageType::Text)));
+                            let tx: oneshot::Sender<Box<[u8]>> =
+                                requests_clone.write().await.remove(&0).unwrap();
+                            _ = tx.send(data);
+                        }
+                        _ => {
+                            // ignore for now
+                        }
+                    }
+                }
+            });
         }
 
         Ok(Self {
-            requests: Arc::new(RwLock::new(HashMap::new())),
+            requests,
             ws,
             next_id: 0,
         })
     }
 
-    pub async fn send_request(&mut self, msg: &str) -> Result<String> {
+    pub async fn send_request(&mut self, msg: &[u8]) -> Result<String> {
         let request_id = self.next_id;
         self.next_id += 1;
 
-        self.ws.send(msg).await?;
+        self.ws.write().await.send(msg).await?;
 
         let (tx, rx) = oneshot::channel();
 
@@ -59,14 +62,6 @@ impl RpcClient {
         drop(requests);
 
         let response = rx.await.unwrap();
-        if let Event::Data { ty, data } = response {
-            assert!(matches!(ty, DataType::Complete(MessageType::Text)));
-            return Ok(String::from_utf8(data.to_vec()).unwrap());
-        }
-        io_err!(InvalidData, "invalid response")
-    }
-
-    pub async fn close(self) -> Result<()> {
-        self.ws.close(()).await
+        Ok(std::str::from_utf8(&*response).unwrap().to_string())
     }
 }
