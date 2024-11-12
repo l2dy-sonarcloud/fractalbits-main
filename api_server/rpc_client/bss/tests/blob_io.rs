73ee02530ed1aaefb0d6307d54d80ef97b4db94a
Thomas <thomas.fractalbits@gmail.com>
2024-11-12 11:24:21 -0800

    bss: move blob storage server's blobs into "data/bss/"
    λ  playground git:(main) ✗ ls data/bss
    019321d5-15e2-73e0-b869-fcfb68ec5f08

--- api_server/rpc_client/bss/tests/blob_io.rs
+++ api_server/rpc_client/bss/tests/blob_io.rs
@@ -9,18 +9,18 @@ async fn test_basic_blob_io() {
     let url = "127.0.0.1:9225";
     tracing::debug!(%url);
     // Skip testing if blob storage server is not up
-    if let Ok(rpc_client) = rpc_client::RpcClient::new(url).await {
+    if let Ok(rpc_client) = RpcClient::new(url).await {
         let header_len = message::MessageHeader::encode_len();
         let blob_id = Uuid::now_v7();
         let content = Bytes::from("42");
         let mut readback_content = Bytes::new();
         let content_len = content.len();
-        let size = nss_put_blob(&rpc_client, blob_id.clone(), content.clone())
+        let size = rpc::bss_put_blob(&rpc_client, blob_id.clone(), content.clone())
             .await
             .unwrap();
         assert_eq!(header_len + content_len, size);
 
-        let size = nss_get_blob(&rpc_client, blob_id, &mut readback_content)
+        let size = rpc::bss_get_blob(&rpc_client, blob_id, &mut readback_content)
             .await
             .unwrap();
         assert_eq!(header_len + content_len, size);
