bd34976842442a652c0d5ab545593dc99eb4c60e
Thomas <thomas.fractalbits@gmail.com>
2024-11-18 06:31:18 -0800

    xtask: fix uuid generating
    λ  playground git:(main) ✗ cargo xtask tool gen_uuids -n 10000000
        Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.03s
         Running `target/debug/xtask tool gen_uuids -n 10000000`
    [INFO ] Generating 10000000 uuids into file uuids.data
    [INFO ] File uuids.data is ready
    λ  playground git:(main) ✗ wc -l uuids.data
    10000000 uuids.data

--- xtask/src/cmd_tool.rs
+++ xtask/src/cmd_tool.rs
@@ -1,5 +1,6 @@
 use cmd_lib::*;
 use rayon::prelude::*;
+use std::io::prelude::*;
 use std::sync::Arc;
 use std::sync::Mutex;
 use uuid::Uuid;
@@ -11,19 +12,35 @@ pub fn run_cmd_tool(ToolKind::GenUuids { num, file }: ToolKind) -> CmdResult {
     info!("Generating {num} uuids into file {file}");
     let num_threads = num_cpus::get();
     let num_uuids = (num as usize) / num_threads;
+    let last_num_uuids = num - (num_uuids as u32) * (num_threads as u32 - 1);
 
     let uuids = Arc::new(Mutex::new(Vec::new()));
-    (0..num_threads).into_par_iter().for_each(|_| {
+    (0..num_threads).into_par_iter().for_each(|i| {
         let mut uuids_str = String::new();
-        for _ in 0..num_uuids {
+        let n = if i == num_threads - 1 {
+            last_num_uuids as usize
+        } else {
+            num_uuids
+        };
+        for _ in 0..n {
             uuids_str += &Uuid::now_v7().to_string();
             uuids_str += "\n";
         }
         uuids.lock().unwrap().push(uuids_str);
     });
 
+    std::fs::File::options()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(&file)
+        .expect(&format!("Truncate {file} failed"));
+    let mut f = std::fs::File::options()
+        .append(true)
+        .open(&file)
+        .expect(&format!("Open {file} for writing failed"));
     for uuid in uuids.lock().unwrap().iter() {
-        std::fs::write(&file, uuid)?;
+        f.write_all(uuid.as_bytes())?;
     }
     info!("File {file} is ready");
     Ok(())
