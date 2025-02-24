6038430d44a847038c83a4251513e168abae3b1a
Thomas <thomas.fractalbits@gmail.com>
2025-02-23 17:41:10 -0800

    feat(api_server): add extractor for authorization

--- api_server/src/handler/common/request/extract.rs
+++ api_server/src/handler/common/request/extract.rs
@@ -1,4 +1,5 @@
 pub mod api_command;
 pub mod api_signature;
+pub mod authorization;
 pub mod bucket_name;
 pub mod key;
---
+++ api_server/src/handler/common/request/extract/authorization.rs
@@ -0,0 +1,118 @@
+#![allow(dead_code)]
+use std::collections::HashMap;
+
+use axum::{
+    extract::{rejection::QueryRejection, FromRequestParts},
+    http::{
+        header::{HeaderName, AUTHORIZATION},
+        request::Parts,
+    },
+};
+use chrono::{DateTime, Duration, NaiveDateTime, TimeZone, Utc};
+
+const AWS4_HMAC_SHA256: &str = "AWS4-HMAC-SHA256";
+const X_AMZ_CONTENT_SHA256: HeaderName = HeaderName::from_static("x-amz-content-sha256");
+const X_AMZ_DATE: HeaderName = HeaderName::from_static("x-amz-date");
+const LONG_DATETIME: &str = "%Y%m%dT%H%M%SZ";
+
+pub struct AuthorizationFromReq(pub Option<Authorization>);
+
+pub struct Authorization {
+    pub key_id: String,
+    pub scope: String,
+    pub signed_headers: String,
+    pub signature: String,
+    pub content_sha256: String,
+    pub date: DateTime<Utc>,
+}
+
+impl<S> FromRequestParts<S> for AuthorizationFromReq
+where
+    S: Send + Sync,
+{
+    type Rejection = QueryRejection;
+
+    // TODO: better error handling
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        let authorization = match parts.headers.get(AUTHORIZATION) {
+            Some(auth) => auth.to_str().unwrap(),
+            None => return Ok(Self(None)),
+        };
+
+        let (auth_kind, rest) = authorization
+            .split_once(' ')
+            .expect("Authorization field too short");
+
+        assert_eq!(
+            auth_kind, AWS4_HMAC_SHA256,
+            "Unsupported authorization method"
+        );
+
+        let mut auth_params = HashMap::new();
+        for auth_part in rest.split(',') {
+            let auth_part = auth_part.trim();
+            let eq = auth_part.find('=').unwrap();
+            let (key, value) = auth_part.split_at(eq);
+            auth_params.insert(key.to_string(), value.trim_start_matches('=').to_string());
+        }
+
+        let cred = auth_params
+            .get("Credential")
+            .expect("Could not find Credential in Authorization field");
+        let signed_headers = auth_params
+            .get("SignedHeaders")
+            .expect("Could not find SignedHeaders in Authorization field")
+            .to_string();
+        let signature = auth_params
+            .get("Signature")
+            .expect("Could not find Signature in Authorization field")
+            .to_string();
+
+        let content_sha256 = parts
+            .headers
+            .get(X_AMZ_CONTENT_SHA256)
+            .expect("Missing x-amz-content-sha256 field");
+
+        let date = parts
+            .headers
+            .get(X_AMZ_DATE)
+            .expect("Missing x-amz-date field")
+            .to_str()
+            .unwrap();
+        let date = parse_date(date);
+
+        assert!(Utc::now() - date <= Duration::hours(24), "Date is too old");
+        let (key_id, scope) = parse_credential(cred);
+        let auth = Authorization {
+            key_id,
+            scope,
+            signed_headers,
+            signature,
+            content_sha256: content_sha256.to_str().unwrap().to_string(),
+            date,
+        };
+        Ok(Self(Some(auth)))
+    }
+}
+
+fn parse_date(date: &str) -> DateTime<Utc> {
+    let date: NaiveDateTime =
+        NaiveDateTime::parse_from_str(date, LONG_DATETIME).expect("Invalid date");
+    Utc.from_utc_datetime(&date)
+}
+
+fn parse_credential(cred: &str) -> (String, String) {
+    let first_slash = cred
+        .find('/')
+        .expect("Credentials does not contain '/' in authorization field");
+    let (key_id, scope) = cred.split_at(first_slash);
+    (
+        key_id.to_string(),
+        scope.trim_start_matches('/').to_string(),
+    )
+}
+
+// TODO: unit test with format like
+// "authorization": "AWS4-HMAC-SHA256 Credential=test_api_key/20250222/fractalbits-integ-tests/s3/aws4_request,
+// SignedHeaders=content-length;content-type;host;x-amz-content-sha256;x-amz-date;x-amz-user-agent,
+// Signature=58a81f5f7074d7ff990b87ce25d1fedf0a54fa3011196ab51c8342bc7609db95",
